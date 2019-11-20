db2a43d9b2087dc25aa493d8e48e5418d08de9e4
Tobi PC <Tobias20.3@hotmail.de>
2019-11-20 17:33:46 +0100

    Basic Movement

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody rb;
+    [Header("Movement Settings")]
+    public float acceleration = 1;
+    public float friction = 1; //not implemented yet
+    [Header("Debugging")]
+    public Vector2 velocity;
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        velocity += new Vector2(Input.GetAxis("Horizontal") * acceleration, Input.GetAxis("Vertical") * acceleration); //Add Acceleration to current Velocity
+        transform.localPosition += new Vector3(velocity.x,0,velocity.y) * Time.deltaTime; //Apply Velocity to Position
+    }
+}
---
+++ Assets/Scripts/rotatesScr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatesScr : MonoBehaviour
+{
+    public float mp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(Input.GetAxis("Vertical")* mp, 0, Input.GetAxis("Horizontal")* -mp);
+    }
+}
