f0d3adba12a8ac1c543a3ea9017383d8c605bf43
Tobi PC <Tobias20.3@hotmail.de>
2019-11-21 20:37:23 +0100

    Updated to 2019.2.13f1 and added friction

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -8,18 +8,26 @@ public class BallScript : MonoBehaviour
     public Rigidbody rb;
     [Header("Movement Settings")]
     public float acceleration = 1;
-    public float friction = 1; //not implemented yet
+    public float friction = 1;
     [Header("Debugging")]
     public Vector2 velocity;
 
     void Update()
     {
         Movement();
+        checkErrors();
     }
 
     void Movement()
     {
         velocity += new Vector2(Input.GetAxis("Horizontal") * acceleration, Input.GetAxis("Vertical") * acceleration); //Add Acceleration to current Velocity
-        transform.localPosition += new Vector3(velocity.x,0,velocity.y) * Time.deltaTime; //Apply Velocity to Position
+        velocity = Vector3.MoveTowards(velocity, Vector3.zero, friction); //Apply Friction
+        transform.localPosition += new Vector3(velocity.x,0,velocity.y) * Time.deltaTime; //Add Velocity to Position
+    }
+
+    void checkErrors()
+    {
+        if (friction >= acceleration)
+            Debug.LogError("Acceleration ("+ acceleration + ") should be larger than friction ("+ friction + ") !");
     }
 }
