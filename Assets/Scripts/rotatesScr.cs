c2920e47eb65183ee63039e41e91ffb74af4d321
Tobi PC <Tobias20.3@hotmail.de>
2019-12-04 20:03:37 +0100

    Improved User Input and added all holes to the level

--- Assets/Scripts/rotatesScr.cs
+++ Assets/Scripts/rotatesScr.cs
@@ -7,7 +7,8 @@ public class rotatesScr : MonoBehaviour
     public float tiltMultiplier;
 
     void Update()
-    {
-        transform.rotation = Quaternion.Euler(Input.GetAxis("Horizontal") * -tiltMultiplier, 0, Input.GetAxis("Vertical")* -tiltMultiplier); 
+    { 
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal") * -tiltMultiplier, 0, Input.GetAxis("Vertical") * -tiltMultiplier);
+        transform.rotation = Quaternion.Euler(input); 
     }
 }
